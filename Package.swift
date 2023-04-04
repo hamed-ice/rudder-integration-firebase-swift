80005e5f783854a12c9e074d8a53997bd0d0415f
Hamed <akhlaghi65@gmail.com>
2023-04-04 11:51:14 +0330

    Packages.swift added

--- Package.swift
+++ Package.swift
@@ -1,4 +1,6 @@
 
+// swift-tools-version:5.5
+// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
@@ -6,7 +8,7 @@ let package = Package(
     name: "RudderFirebase",
     dependencies: [
       .package(name: "Rudder", url: "https://github.com/rudderlabs/rudder-sdk-ios.git", from: "1.8.0"),
-      .package(name: "Firebase", url: "https://github.com/firebase/firebase-ios-sdk.git", from: "10.8.0"),
+      .package(name: "Firebase", url: "https://github.com/firebase/firebase-ios-sdk.git", from: "10.0.0"),
         // add any other dependencies you need here
     ],
     targets: [
